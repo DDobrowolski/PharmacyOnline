52f39391611f2a7e4a394cc89c4ed41cf517a424
Damian <39273100+DDobrowolski@users.noreply.github.com>
2018-08-23 16:58:08 +0200

    Update ViewsController.java

--- Back-end/src/main/java/com/damek/pharmacy/controller/ViewsController.java
+++ Back-end/src/main/java/com/damek/pharmacy/controller/ViewsController.java
@@ -2,8 +2,6 @@ package com.damek.pharmacy.controller;
 
 import java.security.Principal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewsController {
-	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @RequestMapping({ "/**/{[path:[^\\.]*}" })
     public String index() {
